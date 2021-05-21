6394df0cf095163649a8442031f80d32c0b24cca
ErmarTanglao <ermartanglaomm17@outlook.com>
2021-05-21 16:20:51 -0400

    Added Camera Follow Player

---
+++ GAMAGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPostion = target.position + offset;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPostion, smoothSpeed);
+        transform.position = smoothedPostion;
+
+        // transform.LookAt(target);
+    }
+}
